7fc4c1e8f1ccccdd6b67d79ed43bd84d56ff0b63
Juan Manuel Rosello <jmrosello@gmail.com>
2020-07-06 23:22:56 -0300

    Correción botones sugeridos
    agregado el 1er boton dinamico

--- gifOS/js/giphy.js
+++ gifOS/js/giphy.js
@@ -118,14 +118,12 @@ function getTagsRelated(search) {
 
             for (let i = 0 ; i < 3; i++) {
                 tagRelated[i].style.visibility = "visible" 
-                if (i > 0)
-                {
-                    botonRelated = tagRelated[i].childNodes[0];
-                    
-                    botonRelated.innerHTML = JSON.stringify(tagsRelacionados[i].name).replace(/['"]+/g, '') 
-                    botonRelated.onclick = () => {
-                        getSearchResults(JSON.stringify(tagsRelacionados[i].name).replace(/['"]+/g, ''));
-                    };  
+                botonRelated = tagRelated[i].childNodes[0];
+                botonRelated.innerHTML = JSON.stringify(tagsRelacionados[i].name).replace(/['"]+/g, '') 
+                
+                botonRelated.onclick = () => {
+                    getSearchResults(JSON.stringify(tagsRelacionados[i].name).replace(/['"]+/g, ''));
+                };  
                 }
-            }});
+            });
 }
